5b305ac60e5f71f7049477d2e10b7f357e7cac9d
Kirill Adamenkov <i@kiruix.ru>
2024-02-01 22:22:44 +0300

    add fixture cart

--- tests/test_shop.py
+++ tests/test_shop.py
@@ -1,10 +1,13 @@
 import pytest
-from models import Product
+from models import Product, Cart
 
 @pytest.fixture
 def product():
     return Product("book", 100, "This is a book", 1000)
 
+@pytest.fixture
+def cart():
+    return Cart()
 
 class TestProduct:
     def test_product_check_quantity(self, product):
